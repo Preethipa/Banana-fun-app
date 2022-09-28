801a6960b378d84bba5461c36d04be6dc61faa18
PREETHI <preethirajavel06@gmail.com>
2022-09-28 19:50:21 +0530

    wiring input

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("output");
 
 console.log(txtInput);
 
